1f31e3e32804baab1d547cf6c6fccf885c4bce7a
James Dixon <jdixon@pentaho.com>
2011-05-09 09:58:43 -0400

    Added localization for data access client api types

--- package-res/resources/web/messages/nls/messages.js
+++ package-res/resources/web/messages/nls/messages.js
@@ -1,4 +1,62 @@
 {
+
+    // messagebundleid is the id of this message bundle - do not localize this
+    messagebundleid : 'DATAACCESS', 
+    AGG_TYPES_AVERAGE : 'Average',
+    AGG_TYPES_MINIMUM : 'Minimum',
+    AGG_TYPES_MAXIMUM : 'Maximum',
+    AGG_TYPES_SUM : 'Sum',
+    AGG_TYPES_COUNT : 'Count',
+    AGG_TYPES_COUNT_DISTINCT : 'Count Distinct',
+    AGG_TYPES_NONE : 'None',
+    AGG_TYPES_VAR : 'Variance',
+    AGG_TYPES_STDDEV : 'Standard Deviation',
+    AGG_TYPES_CALC : 'Calc',
+    AGG_TYPES_UNKNOWN : 'Unknown',
+    AGG_TYPES_NONE : 'None'
+
+    AXIS_LOCATION_ACROSS : 'Across',
+    AXIS_LOCATION_DOWN : 'Sown',
+    AXIS_LOCATION_SLICER : 'Slicer',
+
+    SORT_TYPES_ASCENDING : "Ascending",
+    SORT_TYPES_DESCENDING : "Descending",
+
+    OPERATOR_TYPES_OR : 'or',
+    OPERATOR_TYPES_AND : 'and',
+
+    CONDITION_TYPES_LIKE : 'like',
+    CONDITION_TYPES_BEGINSWITH : 'begins with',
+    CONDITION_TYPES_ENDSWITH : 'ends with',
+    CONDITION_TYPES_CONTAINS : 'contains',
+    CONDITION_TYPES_NOT_CONTAINS : 'does not contain',
+    CONDITION_TYPES_EQUAL : 'equals',
+    CONDITION_TYPES_LESS_THAN : 'less than',
+    CONDITION_TYPES_LESS_THAN_OR_EQUAL : 'less than or equals',
+    CONDITION_TYPES_MORE_THAN : 'more than',
+    CONDITION_TYPES_MORE_THAN_OR_EQUAL : 'more than or equals',
+    CONDITION_TYPES_IS_NULL : 'is null',
+    CONDITION_TYPES_NOT_NULL : 'is not null',
+
+    DATA_TYPES_NUMERIC : "NUMERIC",
+    DATA_TYPES_STRING : "STRING",
+    DATA_TYPES_DATE : "DATE",
+    DATA_TYPES_BOOLEAN : "BOOLEAN",
+    DATA_TYPES_UNKNOWN : "UNKNOWN",
+    DATA_TYPES_NONE : "NONE",
+
+    ELEMENT_TYPES_CATEGORY : "CATEGORY",
+    ELEMENT_TYPES_CUBE : "CUBE",
+    ELEMENT_TYPES_DIMENSION : "DIMENSION",
+    ELEMENT_TYPES_HIERARCHY : "HIERARCHY",
+    ELEMENT_TYPES_LEVEL : "LEVEL",
+    ELEMENT_TYPES_MEMBER : "MEMBER",
+    ELEMENT_TYPES_FACT : "FACT",
+    ELEMENT_TYPES_ATTRIBUTE : "ATTRIBUTE",
+    ELEMENT_TYPES_KEY : "KEY",
+    ELEMENT_TYPES_QUERY : "QUERY",
+    ELEMENT_TYPES_UNKNOWN : "UNKNOWN",
+    
   EXACTLY_MATCHES: "Exactly matches",
   CONTAINS: "Contains",
   DOES_NOT_CONTAIN: "Doesn't contain",
