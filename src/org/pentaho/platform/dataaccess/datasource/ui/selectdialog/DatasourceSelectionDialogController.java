9e453ef038f26a0bdfbf6bd512e9d74d00277f2f
Nick Baker <codeoncoffee@gmail.com>
2011-08-23 14:49:37 -0400

    [BISERVER-6414] Manage Datasource edit button gets out of sync with contents.

--- src/org/pentaho/platform/dataaccess/datasource/ui/selectdialog/DatasourceSelectionDialogController.java
+++ src/org/pentaho/platform/dataaccess/datasource/ui/selectdialog/DatasourceSelectionDialogController.java
@@ -249,15 +249,15 @@ public class DatasourceSelectionDialogController extends AbstractXulDialogContro
 
       public void success(final List<LogicalModelSummary> logicalModelSummaries) {
         Collections.sort(logicalModelSummaries);
+
+        datasourceSelectionDialogModel.setSelectedIndex(-1);
         datasourceSelectionDialogModel.setLogicalModelSummaries(logicalModelSummaries);
-        datasourceListbox.setSelectedIndex(-1);
+  
         if (domainId != null && modelId != null) {
           datasourceSelectionDialogModel.setSelectedLogicalModel(domainId, modelId);
           datasourceListbox.setSelectedIndex(datasourceSelectionDialogModel.getSelectedIndex());
-        } else if (logicalModelSummaries.isEmpty()) {
-          
         } else {
-          datasourceListbox.setSelectedIndex(0);
+          datasourceSelectionDialogModel.setSelectedIndex(0);
         }
       }
 
