b6d7c72476a6c96c63d762540175a3b5a6b0ac63
Tyler Band <tband@pentaho.com>
2012-06-29 16:38:45 -0400

    added new method importAnalysisSchemaFile with multi-part form params. Store the inputStream (MondrianXML) directly to repository

--- src/org/pentaho/platform/dataaccess/datasource/wizard/service/impl/AnalysisDatasourceService.java
+++ src/org/pentaho/platform/dataaccess/datasource/wizard/service/impl/AnalysisDatasourceService.java
@@ -23,8 +23,11 @@ package org.pentaho.platform.dataaccess.datasource.wizard.service.impl;
 
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
@@ -40,64 +43,143 @@ import org.pentaho.platform.api.repository2.unified.IUnifiedRepository;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.plugin.action.mondrian.catalog.IMondrianCatalogService;
+import org.pentaho.platform.plugin.services.importer.IPlatformImportBundle;
+import org.pentaho.platform.plugin.services.importer.IPlatformImporter;
+import org.pentaho.platform.plugin.services.importer.RepositoryFileImportBundle;
 import org.pentaho.platform.plugin.services.importexport.legacy.MondrianCatalogRepositoryHelper;
+
+import com.sun.jersey.multipart.FormDataParam;
 
 @Path("/data-access/api/mondrian")
 public class AnalysisDatasourceService {
 
-	private IMondrianCatalogService mondrianCatalogService;
-	private final String ACTION_READ = "org.pentaho.repository.read"; 
-	private final String ACTION_CREATE = "org.pentaho.repository.create";
-	private final String ACTION_ADMINISTER_SECURITY = "org.pentaho.security.administerSecurity";
+  private static final String ACCESS_DENIED = "Access Denied";
 
-	public AnalysisDatasourceService() {
-		super();
-		mondrianCatalogService = PentahoSystem.get(IMondrianCatalogService.class, "IMondrianCatalogService", PentahoSessionHolder.getSession());
-	}
+  private static final String SUCCESS = "SUCCESS";
 
-	@PUT
-	@Path("/import")
-	@Consumes({ TEXT_PLAIN })	
-	@Produces("text/plain")
-	public Response importAnalysisDatasource(String parameters, @QueryParam("analysisFile") String analysisFile, @QueryParam("databaseConnection") String databaseConnection) throws PentahoAccessControlException {
-		try {
-			validateAccess();
-		    String TMP_FILE_PATH = File.separatorChar + "system" + File.separatorChar + "tmp" + File.separatorChar;
-		    String sysTmpDir = PentahoSystem.getApplicationContext().getSolutionPath(TMP_FILE_PATH);
-		    File mondrianFile = new File(sysTmpDir + File.separatorChar + analysisFile);
+  private IMondrianCatalogService mondrianCatalogService;
+  
+  private IPlatformImporter importHandlerService;
 
-		    mondrianCatalogService.importSchema(mondrianFile, databaseConnection, parameters);
-			return Response.ok("SUCCESS").type(MediaType.TEXT_PLAIN).build();
-		} catch (Exception e) {
-			return Response.serverError().entity(e.getMessage()).build();
-		}
-	}
-	
-	@PUT
-	@Path("/addSchema")
-	@Consumes({ MediaType.APPLICATION_OCTET_STREAM, TEXT_PLAIN })
-	@Produces("text/plain")
-	public Response addSchema(InputStream mondrianFile, @QueryParam("catalogName") String catalogName, @QueryParam("datasourceInfo") String datasourceInfo) throws PentahoAccessControlException {
-		try {
-			validateAccess();
-			MondrianCatalogRepositoryHelper helper = new MondrianCatalogRepositoryHelper(PentahoSystem.get(IUnifiedRepository.class));
-		    helper.addSchema(mondrianFile, catalogName, datasourceInfo);
-		    
-		    // Flush the Mondrian cache to show imported datasources. 
-	        IMondrianCatalogService mondrianCatalogService = PentahoSystem.get(IMondrianCatalogService.class, "IMondrianCatalogService", PentahoSessionHolder.getSession());
-		    mondrianCatalogService.reInit(PentahoSessionHolder.getSession());
-		    
-			return Response.ok("SUCCESS").type(MediaType.TEXT_PLAIN).build();
-		} catch(Exception e) {
-			return Response.serverError().entity(e.toString()).build();
-		}
-	}
-	
-	private void validateAccess() throws PentahoAccessControlException {
-		IAuthorizationPolicy policy = PentahoSystem.get(IAuthorizationPolicy.class);
-		boolean isAdmin = policy.isAllowed(ACTION_READ) && policy.isAllowed(ACTION_CREATE) && policy.isAllowed(ACTION_ADMINISTER_SECURITY); 
-		if(!isAdmin) {
-			throw new PentahoAccessControlException("Access Denied");
-		}
-	}	
+  private final String ACTION_READ = "org.pentaho.repository.read";
+
+  private final String ACTION_CREATE = "org.pentaho.repository.create";
+
+  private final String ACTION_ADMINISTER_SECURITY = "org.pentaho.security.administerSecurity";
+
+  private static final String DOMAIN_ID = "domain-id";
+
+  private static final String TEXT_XMI_XML = "text/xmi+xml";
+
+  private static final String UTF_8 = "UTF-8";
+
+  private static final String mondrianMimeType = "application/vnd.pentaho.mondrian+xml";
+
+  public AnalysisDatasourceService() {
+    super();
+    mondrianCatalogService = PentahoSystem.get(IMondrianCatalogService.class, "IMondrianCatalogService",
+        PentahoSessionHolder.getSession());
+    
+  }
+
+  @PUT
+  @Path("/import")
+  @Consumes({ TEXT_PLAIN })
+  @Produces("text/plain")
+  public Response importAnalysisDatasource(String parameters, @QueryParam("analysisFile")
+  String analysisFile, @QueryParam("databaseConnection")
+  String databaseConnection) throws PentahoAccessControlException {
+    try {
+      validateAccess();
+      String TMP_FILE_PATH = File.separatorChar + "system" + File.separatorChar + "tmp" + File.separatorChar;
+      String sysTmpDir = PentahoSystem.getApplicationContext().getSolutionPath(TMP_FILE_PATH);
+      File mondrianFile = new File(sysTmpDir + File.separatorChar + analysisFile);
+
+      mondrianCatalogService.importSchema(mondrianFile, databaseConnection, parameters);
+      return Response.ok(SUCCESS).type(MediaType.TEXT_PLAIN).build();
+    } catch (Exception e) {
+      return Response.serverError().entity(e.getMessage()).build();
+    }
+  }
+
+  @PUT
+  @Path("/importSchema")
+  @Consumes(MediaType.MULTIPART_FORM_DATA)
+  @Produces("text/plain")
+  public Response importAnalysisSchemaFile(
+      @FormDataParam("parameters") String parameters, 
+      @FormDataParam("schemaFile") InputStream dataInputStream, 
+      @FormDataParam("datasourceName") String datasourceName,
+      @FormDataParam("overwrite") String overwrite,
+      @FormDataParam("xmlaEnabledFlag") String xmlaEnabledFlag
+      ) throws PentahoAccessControlException {
+    try {     
+      validateAccess();
+      boolean overWriteInRepository = "True".equalsIgnoreCase(overwrite)?true:false;
+      boolean xmlaEnabled = "True".equalsIgnoreCase(xmlaEnabledFlag)?true:false;
+     
+      File mondrianFile = createFileFromStream(dataInputStream);
+      mondrianCatalogService.importSchema(mondrianFile, datasourceName, parameters);
+      return Response.ok(SUCCESS).type(MediaType.TEXT_PLAIN).build();
+    } catch (Exception e) {
+      return Response.serverError().entity(e.getMessage()).build();
+    }
+  }
+
+  private File createFileFromStream(InputStream dataInputStream) throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(dataInputStream));
+    String currentLine = null;
+    while ((currentLine = in.readLine()) != null) {
+      currentLine += currentLine;
+    }
+    File mondrianFile = new File(currentLine);
+    return mondrianFile;
+  }
+
+  @PUT
+  @Path("/addSchema")
+  @Consumes({ MediaType.APPLICATION_OCTET_STREAM, TEXT_PLAIN })
+  @Produces("text/plain")
+  public Response addSchema(InputStream mondrianFile, @QueryParam("catalogName")
+  String catalogName, @QueryParam("datasourceInfo")
+  String datasourceInfo) throws PentahoAccessControlException {
+    try {
+      validateAccess();
+      MondrianCatalogRepositoryHelper helper = new MondrianCatalogRepositoryHelper(
+          PentahoSystem.get(IUnifiedRepository.class));
+      helper.addSchema(mondrianFile, catalogName, datasourceInfo);
+
+      // Flush the Mondrian cache to show imported datasources. 
+      IMondrianCatalogService mondrianCatalogService = PentahoSystem.get(IMondrianCatalogService.class,
+          "IMondrianCatalogService", PentahoSessionHolder.getSession());
+      mondrianCatalogService.reInit(PentahoSessionHolder.getSession());
+
+      return Response.ok(SUCCESS).type(MediaType.TEXT_PLAIN).build();
+    } catch (Exception e) {
+      return Response.serverError().entity(e.toString()).build();
+    }
+  }
+
+  private void validateAccess() throws PentahoAccessControlException {
+    IAuthorizationPolicy policy = PentahoSystem.get(IAuthorizationPolicy.class);
+    boolean isAdmin = policy.isAllowed(ACTION_READ) && policy.isAllowed(ACTION_CREATE)
+        && policy.isAllowed(ACTION_ADMINISTER_SECURITY);
+    if (!isAdmin) {
+      throw new PentahoAccessControlException(ACCESS_DENIED);
+    }
+  }
+
+  /**
+   * Utility to build a bundle from the data input stream
+   * @param dataInputStream
+   * @param domainId
+   * @return IPlatformImportBundle cast
+   */
+  private IPlatformImportBundle fileIImportBundle(InputStream dataInputStream, String domainId,String mimeType,boolean overWriteInRepository) {
+    RepositoryFileImportBundle.Builder bundleBuilder = new RepositoryFileImportBundle.Builder().input(dataInputStream)
+        .charSet(UTF_8).hidden(false).mime(mimeType).withParam(DOMAIN_ID, domainId).overwrite(overWriteInRepository);
+   
+    return (IPlatformImportBundle) bundleBuilder;
+
+  }
 }
