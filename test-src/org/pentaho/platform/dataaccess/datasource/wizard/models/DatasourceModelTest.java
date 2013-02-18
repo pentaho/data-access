39a1cf56aae8fb21619b6a43001a2dbd296eb8ba
Kris Rivera <krivera@pentaho.com>
2013-02-18 13:59:04 -0500

    Set object factory to allow DatasourceModelTest to pass.

--- test-src/org/pentaho/platform/dataaccess/datasource/wizard/models/DatasourceModelTest.java
+++ test-src/org/pentaho/platform/dataaccess/datasource/wizard/models/DatasourceModelTest.java
@@ -33,14 +33,16 @@ import org.pentaho.platform.dataaccess.datasource.DatasourceType;
 import org.pentaho.platform.dataaccess.datasource.beans.BusinessData;
 import org.pentaho.platform.dataaccess.datasource.wizard.service.agile.CsvTransformGeneratorTest;
 import org.pentaho.platform.dataaccess.datasource.wizard.service.impl.InMemoryDSWDatasourceServiceImpl;
+import org.pentaho.platform.engine.core.TestObjectFactory;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
 
 @SuppressWarnings("nls")
 public class DatasourceModelTest {
-  
+
   @Test
   public void test() {
     DatasourceModel datasourceModel = new DatasourceModel();
-//    Assert.assertNull(datasourceModel.getCsvModel().getSelectedFile());
+    //    Assert.assertNull(datasourceModel.getCsvModel().getSelectedFile());
     Assert.assertNull(datasourceModel.getQuery());
     assertNull(datasourceModel.getModelInfo().getFileInfo().getTmpFilename());
     Assert.assertEquals(DatasourceType.NONE, datasourceModel.getDatasourceType());
@@ -48,21 +50,21 @@ public class DatasourceModelTest {
     Assert.assertEquals(false, datasourceModel.isValidated());
     datasourceModel.setGuiStateModel(contructRelationalModel(datasourceModel.getGuiStateModel()));
     Assert.assertEquals(true, datasourceModel.isValidated());
-    datasourceModel.setDatasourceType(DatasourceType.CSV);    
+    datasourceModel.setDatasourceType(DatasourceType.CSV);
     Assert.assertEquals(false, datasourceModel.isValidated());
-//    datasourceModel.setCsvModel(constructCsvModel(datasourceModel.getCsvModel()));    
-    Assert.assertEquals(false, datasourceModel.isValidated());    
+    //    datasourceModel.setCsvModel(constructCsvModel(datasourceModel.getCsvModel()));    
+    Assert.assertEquals(false, datasourceModel.isValidated());
     datasourceModel.setModelInfo(CsvTransformGeneratorTest.createModel());
     assertEquals(false, datasourceModel.isValidated());
   }
-  
+
   private GuiStateModel contructRelationalModel(GuiStateModel guiStateModel) {
     IDatabaseConnection connection = new DatabaseConnection();
     connection.setAccessType(DatabaseAccessType.NATIVE);
-//    connection.setDriverClass("org.hsqldb.jdbcDriver");
+    //    connection.setDriverClass("org.hsqldb.jdbcDriver");
     connection.setName("SampleData");
     connection.setPassword("password");
-//    connection.setUrl("jdbc:hsqldb:file:test-res/solution1/system/data/sampledata");
+    //    connection.setUrl("jdbc:hsqldb:file:test-res/solution1/system/data/sampledata");
     connection.setUsername("pentaho_user");
     List<IDatabaseConnection> connectionList = new ArrayList<IDatabaseConnection>();
     connectionList.add(connection);
@@ -85,7 +87,7 @@ public class DatasourceModelTest {
     dataSample.add(rowData);
 
     String locale = LocaleHelper.getLocale().toString();
-    
+
     SqlPhysicalModel model = new SqlPhysicalModel();
     SqlDataSource dataSource = new SqlDataSource();
     dataSource.setDatabaseName("SampleData");
@@ -94,38 +96,38 @@ public class DatasourceModelTest {
     model.getPhysicalTables().add(table);
     table.setTargetTableType(TargetTableType.INLINE_SQL);
     table.setTargetTable("select * from customers");
-    
+
     SqlPhysicalColumn column = new SqlPhysicalColumn(table);
     column.setTargetColumn("customername");
     column.setName(new LocalizedString(locale, "Customer Name"));
     column.setDescription(new LocalizedString(locale, "Customer Name Desc"));
     column.setDataType(DataType.STRING);
-    
+
     table.getPhysicalColumns().add(column);
-    
+
     LogicalModel logicalModel = new LogicalModel();
     model.setId("MODEL");
     model.setName(new LocalizedString(locale, "My Model"));
     model.setDescription(new LocalizedString(locale, "A Description of the Model"));
-    
+
     LogicalTable logicalTable = new LogicalTable();
     logicalTable.setPhysicalTable(table);
-    
+
     logicalModel.getLogicalTables().add(logicalTable);
-    
+
     LogicalColumn logicalColumn = new LogicalColumn();
     logicalColumn.setId("LC_CUSTOMERNAME");
     logicalColumn.setPhysicalColumn(column);
-    
+
     logicalTable.addLogicalColumn(logicalColumn);
-    
+
     Category mainCategory = new Category();
     mainCategory.setId("CATEGORY");
     mainCategory.setName(new LocalizedString(locale, "Category"));
     mainCategory.addLogicalColumn(logicalColumn);
-    
+
     logicalModel.getCategories().add(mainCategory);
-    
+
     Domain domain = new Domain();
     domain.addPhysicalModel(model);
     domain.addLogicalModel(logicalModel);
@@ -143,20 +145,19 @@ public class DatasourceModelTest {
   @Test
   public void testGenerateTablename() throws Exception {
     DatasourceModel model = new DatasourceModel();
-    
+
     model.setDatasourceName("ABcdef$GhijkLmNopqRSTuvwxy&z 1234567890");
     assertEquals("abcdefghijklmnopqrstuvwxyz_1234567890", model.generateTableName());
-    
+
     model.setDatasourceName("!@#$%^&*()=+?><';:.|{}~`");
     assertEquals("", model.generateTableName());
   }
-  
-  @Test(expected=IllegalStateException.class)
-  public void testGenerateTablename_nullDatasourceName() throws Exception {
-     DatasourceModel model = new DatasourceModel();
-     model.generateTableName();
-  }
 
+  @Test(expected = IllegalStateException.class)
+  public void testGenerateTablename_nullDatasourceName() throws Exception {
+    DatasourceModel model = new DatasourceModel();
+    model.generateTableName();
+  }
 
   @Test
   public void testRelationalDatasourceDTO() throws Exception {
@@ -178,10 +179,10 @@ public class DatasourceModelTest {
 
     IDatabaseConnection connection = new DatabaseConnection();
     connection.setAccessType(DatabaseAccessType.NATIVE);
-//    connection.setDriverClass("org.hsqldb.jdbcDriver");
+    //    connection.setDriverClass("org.hsqldb.jdbcDriver");
     connection.setName("SampleData");
     connection.setPassword("password");
-//    connection.setUrl("jdbc:hsqldb:file:test-res/solution1/system/data/sampledata");
+    //    connection.setUrl("jdbc:hsqldb:file:test-res/solution1/system/data/sampledata");
     connection.setUsername("pentaho_user");
     datasourceModel2.getGuiStateModel().setConnections(Collections.singletonList(connection));
 
@@ -190,13 +191,15 @@ public class DatasourceModelTest {
     assertEquals(datasourceModel.getDatasourceName(), datasourceModel2.getDatasourceName());
     assertEquals(datasourceModel.getDatasourceType(), datasourceModel2.getDatasourceType());
     assertEquals(datasourceModel.getQuery(), datasourceModel2.getQuery());
-    assertEquals(datasourceModel.getSelectedRelationalConnection().getName(), datasourceModel2.getSelectedRelationalConnection().getName());
+    assertEquals(datasourceModel.getSelectedRelationalConnection().getName(), datasourceModel2
+        .getSelectedRelationalConnection().getName());
 
   }
 
-
   @Test
   public void testDatasourceDTOSerialization() throws Exception {
+
+    PentahoSystem.setObjectFactory(new TestObjectFactory());
 
     DatasourceModel datasourceModel = new DatasourceModel();
     datasourceModel.setDatasourceName("testDatasource");
