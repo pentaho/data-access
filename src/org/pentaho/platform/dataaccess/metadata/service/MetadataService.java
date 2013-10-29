c1fc9e4245fde1fdc9d22030c11537535212dfd7
wseyler <wseyler@pentaho.com>
2013-10-29 11:07:01 -0400

    BISERVER-10435: Added tags so that enunciate will pick up serialized structures.

--- src/org/pentaho/platform/dataaccess/metadata/service/MetadataService.java
+++ src/org/pentaho/platform/dataaccess/metadata/service/MetadataService.java
@@ -19,8 +19,6 @@ package org.pentaho.platform.dataaccess.metadata.service;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
-import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
-import  javax.ws.rs.QueryParam;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -32,6 +30,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
