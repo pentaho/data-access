e326b555777bd15284545415fa10bb67528b3187
pminutillo <pminutillo@pentaho.com>
2013-11-07 16:16:14 -0500

    BISERVER-10378 - Added tests for DatasourceProvider, DatasourceChild, impl for DatasourceType

---
+++ src/org/pentaho/platform/dataaccess/catalog/impl/DatasourceType.java
@@ -0,0 +1,75 @@
+/**
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright 2006 - 2013 Pentaho Corporation.  All rights reserved.
+ *
+ */
+
+package org.pentaho.platform.dataaccess.catalog.impl;
+
+import org.pentaho.platform.dataaccess.catalog.api.IDatasourceType;
+
+import java.util.Locale;
+
+public class DatasourceType implements IDatasourceType {
+
+  String id;
+  String displayName;
+
+  /**
+   *
+   * @return
+   */
+  @Override
+  public String getId() {
+    return id;
+  }
+
+  /**
+   *
+   * @param locale
+   * @return
+   */
+  @Override
+  public String getDisplayName( Locale locale ) {
+    return displayName;
+  }
+
+  /**
+   *
+   * @param id
+   */
+  public void setId( String id ) {
+    this.id = id;
+  }
+
+  /**
+   *
+   * @param displayName
+   * @return
+   */
+  public void setDisplayName( String displayName ) {
+    this.displayName = displayName;
+  }
+
+  /**
+   *
+   * @param id
+   * @param displayName
+   */
+  public DatasourceType( String id, String displayName ) {
+    this.id = id;
+    this.displayName = displayName;
+  }
+}
