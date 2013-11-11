30f4b0b94de8c5c0bd4cdbde943d9caa1c3d4049
Ramaiz Mansoor <rmansoor@pentaho.com>
2013-11-11 15:07:36 -0500

    BISERVER-10550 - Updating provider classes

--- src/org/pentaho/platform/dataaccess/datasource/provider/JDBCDatasourceType.java
+++ src/org/pentaho/platform/dataaccess/datasource/provider/JDBCDatasourceType.java
@@ -1,23 +1,12 @@
 package org.pentaho.platform.dataaccess.datasource.provider;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
+import org.pentaho.platform.dataaccess.catalog.impl.DatasourceType;
 
-import org.pentaho.platform.dataaccess.catalog.api.IDatasourceType;
-import org.pentaho.platform.dataaccess.datasource.provider.messages.Messages;;
+public class JDBCDatasourceType extends DatasourceType{
 
-public class JDBCDatasourceType implements IDatasourceType{
-  private static final String ID = "JDBC"; 
-  ResourceBundle resourceBundle;
-  @Override
-  public String getId() {
-    return ID;
+  public JDBCDatasourceType( String id, String displayName ) {
+    super( id, displayName );
   }
 
-  @Override
-  public String getDisplayName( Locale locale ) {
-    resourceBundle = Messages.getInstance().getBundle(locale);
-    return resourceBundle.getString( ID );
-  }
-
+  public static final String ID = "JDBC"; 
 }
