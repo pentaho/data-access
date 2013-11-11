fca36b943d4906c6dc0622e18c9b4c523b4235a1
Ramaiz Mansoor <rmansoor@pentaho.com>
2013-11-11 08:34:56 -0500

    BISERVER-10550 - Adding Datasource Providers for JDBC, Analysis and Metadata

---
+++ src/org/pentaho/platform/dataaccess/datasource/provider/MetadataDatasourceProvider.java
@@ -0,0 +1,34 @@
+package org.pentaho.platform.dataaccess.datasource.provider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.pentaho.metadata.repository.IMetadataDomainRepository;
+import org.pentaho.platform.dataaccess.catalog.api.IDatasource;
+import org.pentaho.platform.dataaccess.catalog.api.IDatasourceProvider;
+import org.pentaho.platform.dataaccess.catalog.api.IDatasourceType;
+import org.pentaho.platform.dataaccess.catalog.impl.Datasource;
+
+public class MetadataDatasourceProvider implements IDatasourceProvider{
+
+  private IMetadataDomainRepository metadataDomainRepository;
+  private IDatasourceType datasourceType = new MetadataDatasourceType();
+  
+  public MetadataDatasourceProvider(final IMetadataDomainRepository metadataDomainRepository) {
+    this.metadataDomainRepository = metadataDomainRepository;
+  }
+  @Override
+  public List<IDatasource> getDatasources() {
+    List<IDatasource> datasources = new ArrayList<IDatasource>();
+    for(String id :metadataDomainRepository.getDomainIds()) {
+      datasources.add( new Datasource(id, getType(), null) );
+    }
+    return datasources;
+  }
+
+  @Override
+  public IDatasourceType getType() {
+    return datasourceType;
+  }
+
+}
