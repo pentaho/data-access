f0d53312c4e8930b79b7812fd00745d5e321928b
Ramaiz Mansoor <rmansoor@pentaho.com>
2013-11-11 13:18:36 -0500

    BISERVER-10550 - Creating a unit test for datasource provider impl

--- src/org/pentaho/platform/dataaccess/datasource/provider/AnalysisDatasourceProvider.java
+++ src/org/pentaho/platform/dataaccess/datasource/provider/AnalysisDatasourceProvider.java
@@ -17,7 +17,7 @@ import org.pentaho.platform.plugin.action.mondrian.catalog.MondrianCube;
 public class AnalysisDatasourceProvider implements IDatasourceProvider{
 
   private MondrianCatalogHelper mondrianCatalogHelper;
-  private IDatasourceType datasourceType = new AnalysisDatasourceType();
+  private IDatasourceType datasourceType = new AnalysisDatasourceType(AnalysisDatasourceType.ID,AnalysisDatasourceType.ID);
   
   public AnalysisDatasourceProvider(final MondrianCatalogHelper mondrianCatalogHelper) {
     this.mondrianCatalogHelper = mondrianCatalogHelper;
