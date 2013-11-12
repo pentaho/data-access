63e72e6bb79f386fff78539e09daf10ea8597099
wseyler <wseyler@pentaho.com>
2013-11-12 14:28:39 -0500

    Formatting Changes

--- src/org/pentaho/platform/dataaccess/datasource/wizard/models/IDSWTemplate.java
+++ src/org/pentaho/platform/dataaccess/datasource/wizard/models/IDSWTemplate.java
@@ -1,3 +1,21 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License, version 2 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ *
+ * Copyright 2006 - 2013 Pentaho Corporation.  All rights reserved.
+ */
+
 package org.pentaho.platform.dataaccess.datasource.wizard.models;
 
 import java.util.Locale;
@@ -12,32 +30,34 @@ public interface IDSWTemplate {
    * @return
    */
   String getID();
-  
+
   /**
    * Get the display name of the model.
+   * 
    * @return
    */
-  String getDisplayName(Locale locale);
-  
+  String getDisplayName( Locale locale );
+
   /**
    * Creates the underlaying domain for the template
    * 
    * @param iDSWDataSource
    */
-  void createDatasource(IDSWDataSource iDSWDataSource, boolean overwrite) throws DSWException;
-  
+  void createDatasource( IDSWDataSource iDSWDataSource, boolean overwrite ) throws DSWException;
+
   /**
    * Serialize the IDSWTemplateModel into a string
-   *  
+   * 
    * @param dswTemplateModel
    * @return
    */
-  String serialize(IDSWTemplateModel dswTemplateModel) throws DSWException;
-  
+  String serialize( IDSWTemplateModel dswTemplateModel ) throws DSWException;
+
   /**
    * Deserializes the IDSWTemplateModel object.
+   * 
    * @param IDSWTemplateModel
    * @return
    */
-  IDSWTemplateModel deserialize(String serializedModel) throws DSWException;
+  IDSWTemplateModel deserialize( String serializedModel ) throws DSWException;
 }
--- src/org/pentaho/platform/dataaccess/datasource/wizard/service/DSWDataSourceWizardService.java
+++ src/org/pentaho/platform/dataaccess/datasource/wizard/service/DSWDataSourceWizardService.java
@@ -1,3 +1,21 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License, version 2 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ *
+ * Copyright 2006 - 2013 Pentaho Corporation.  All rights reserved.
+ */
+
 package org.pentaho.platform.dataaccess.datasource.wizard.service;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -17,23 +35,24 @@ import org.pentaho.platform.engine.core.system.PentahoSystem;
 
 /**
  * This class provides web services associated with the DataSource Wizard Dialog.
+ * 
  * @author tkafalas
- *
+ * 
  */
 
 @Path( "/data-access-v2/api/dsw-wizard/" )
 public class DSWDataSourceWizardService {
-  
+
   @GET
-  @Path("/list")
-  @Produces({APPLICATION_JSON, APPLICATION_XML})
+  @Path( "/list" )
+  @Produces( { APPLICATION_JSON, APPLICATION_XML } )
   public DSWTemplateSummaryListDto getDSWModelList() {
-    IDSWDataSourceWizard dsw = PentahoSystem.get(IDSWDataSourceWizard.class);
+    IDSWDataSourceWizard dsw = PentahoSystem.get( IDSWDataSourceWizard.class );
     List<IDSWTemplate> templates = dsw.getTemplates();
     ArrayList<DSWTemplateSummaryDto> templateNames = new ArrayList<DSWTemplateSummaryDto>();
-    for (IDSWTemplate template : templates) {
-      templateNames.add(new DSWTemplateSummaryDto(template.getID(), template.getDisplayName(Locale.getDefault())));
+    for ( IDSWTemplate template : templates ) {
+      templateNames.add( new DSWTemplateSummaryDto( template.getID(), template.getDisplayName( Locale.getDefault() ) ) );
     }
-    return new DSWTemplateSummaryListDto(templateNames);
+    return new DSWTemplateSummaryListDto( templateNames );
   }
 }
