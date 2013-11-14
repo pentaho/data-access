003fb938c518b6216fec5ef2381678de9300e3dc
Pedro Teixeira <pedrofvteixeira@gmail.com>
2013-11-14 13:47:40 +0000

    data-access plugin refactored into data-access-api and data-acess-core; running “ant install” from root installs plugin in designated bi-server (see build.properties in root); build process places both jars in generated zip and places api in core’s lib folder; in addition, build process also copies data-access-api to biserver’s tomcat WEB-INF/lib

---
+++ data-access-core/src/org/pentaho/platform/dataaccess/metadata/model/api/IColumn.java
@@ -0,0 +1,96 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ */
+
+package org.pentaho.platform.dataaccess.metadata.model.api;
+
+import java.io.Serializable;
+
+/**
+ * Represents a Metadata Column object
+ */
+public interface IColumn extends Serializable {
+
+  /**
+   * Returns the data type of the column
+   * 
+   * @return
+   */
+  public String getType();
+
+  /**
+   * Returns the id of the column
+   * 
+   * @return
+   */
+  public String getId();
+
+  /**
+   * Returns the name of the column
+   * 
+   * @return
+   */
+  public String getName();
+
+  /**
+   * Returns the category of the column
+   * 
+   * @return
+   */
+  public String getCategory();
+
+  /**
+   * Returns the default aggregation type of the column. Can be NONE.
+   * 
+   * @return
+   */
+  public String getDefaultAggType();
+
+  /**
+   * Returns an array of the availble aggregation types. Can be empty.
+   * 
+   * @return
+   */
+  public String[] getAggTypes();
+
+  /**
+   * Returns the currently selected aggregation type. Can be NONE.
+   * 
+   * @return
+   */
+  public String getSelectedAggType();
+
+  /**
+   * Returns the type of this column, e.g. DIMENSION. Can be UNKNOWN.
+   * 
+   * @return
+   */
+  public String getFieldType();
+
+  /**
+   * Returns the horizontal alignment of the column - RIGHT, LEFT, CENTERED.
+   * 
+   * @return
+   */
+  public String getHorizontalAlignment();
+
+  /**
+   * Returns the format mask for this column, e.g. "#,###.00" or "MMM dd, yyyy"
+   * 
+   * @return
+   */
+  public String getFormatMask();
+}
---
+++ data-access-core/src/org/pentaho/platform/dataaccess/metadata/model/api/IQuery.java
@@ -0,0 +1,42 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ */
+
+package org.pentaho.platform.dataaccess.metadata.model.api;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * Represents an MQL Query. Contains a list of selected columns, conditions and order information.
+ * 
+ */
+public interface IQuery extends Serializable {
+
+  public IColumn[] getColumns();
+
+  public ICondition[] getConditions();
+
+  public IOrder[] getOrders();
+
+  public String getDomainName();
+
+  public String getModelId();
+
+  public Map<String, String> getDefaultParameterMap();
+
+  public Boolean getDisableDistinct();
+}
