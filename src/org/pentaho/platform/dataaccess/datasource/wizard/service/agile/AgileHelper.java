a6096043c95bc52e9112bacdf8cd91fb3d6a4e6b
Marc Batchelor <mbatchelor@pentaho.com>
2014-09-26 16:26:27 -0400

    Update AgileHelper.java

--- src/org/pentaho/platform/dataaccess/datasource/wizard/service/agile/AgileHelper.java
+++ src/org/pentaho/platform/dataaccess/datasource/wizard/service/agile/AgileHelper.java
@@ -55,6 +55,8 @@ public class AgileHelper {
       dialect = conn.getMetaData().getDatabaseProductName();
       if ( dialect.indexOf( "HSQL" ) >= 0 ) {
         dialect = "Hypersonic";
+      } else if ( dialect.indexOf( "Microsoft SQL" ) >= 0) {
+        dialect = "MSSQLNATIVE";
       }
       conn.close();
     } catch ( SQLException e ) {
