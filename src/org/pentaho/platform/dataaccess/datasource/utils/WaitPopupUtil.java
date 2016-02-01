05550124e085e41cc406119818a65e1018ad2709
Sergey_Travin <Sergey_Travin@epam.com>
2016-02-01 18:02:28 +0400

    [SP-2446][BISERVER-12829] Errors when integrating Pentaho User Console into an IFrame

--- src/org/pentaho/platform/dataaccess/datasource/utils/WaitPopupUtil.java
+++ src/org/pentaho/platform/dataaccess/datasource/utils/WaitPopupUtil.java
@@ -19,10 +19,10 @@ package org.pentaho.platform.dataaccess.datasource.utils;
 
 public class WaitPopupUtil {
   public static native void showWaitPopup() /*-{
-    window.top.executeCommand("ShowWaitPopupCommand")
+    window.parent.executeCommand("ShowWaitPopupCommand")
   }-*/;
 
   public static native void hideWaitPopup() /*-{
-    window.top.executeCommand("HideWaitPopupCommand")
+    window.parent.executeCommand("HideWaitPopupCommand")
   }-*/;
 }
